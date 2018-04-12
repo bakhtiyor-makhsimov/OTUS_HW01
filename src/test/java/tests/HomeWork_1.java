9a47c3f00857584815102f091ff044633bf7e0dd
Makhsimov <bmakjsimov@am.sony.com>
2018-04-11 17:59:11 -0700

    First commit

---
+++ src/test/java/tests/HomeWork_1.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HomeWork_1 {
+
+    @Test
+    public void test() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://blazedemo.com/index.php");
+        Thread.sleep(2000);
+        WebElement button = driver.findElement(By.xpath("//input[@value='Find Flights']"));
+        button.click();
+        Thread.sleep(2000);
+        WebElement title = driver.findElement(By.xpath("//title[text()='BlazeDemo - reserve']"));
+        String text1 = "BlazeDemo - reserve";
+        System.out.println(title.getText());
+//      assertTrue(title.getText() == text1);
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
